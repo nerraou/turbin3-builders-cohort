3a9258a69c3d51c3dcb0a375dd1de4b3eed5aa71
nerraou <nouhaylaerraou@gmail.com>
2026-05-14 19:14:35 +0100

    feat: create deposit, withdraw and close instraction

--- assignment-3/anchor-vault/programs/anchor-vault/src/lib.rs
+++ assignment-3/anchor-vault/programs/anchor-vault/src/lib.rs
@@ -18,4 +18,16 @@ pub mod anchor_vault {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         ctx.accounts.initialize(&ctx.bumps)
     }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        ctx.accounts.deposit(amount)
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+        ctx.accounts.withdraw(amount)
+    }
+
+    pub fn close(ctx: Context<Close>) -> Result<()> {
+        ctx.accounts.close()
+    }
 }
