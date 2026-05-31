c7a397dfca50e962bbeb64877b812224d51d8ea0
nerraou <nouhaylaerraou@gmail.com>
2026-05-31 21:30:19 +0100

    feat: create initialize and list instructions

--- assignment-7/anchor-marketplace-core/programs/anchor-marketplace-core/src/lib.rs
+++ assignment-7/anchor-marketplace-core/programs/anchor-marketplace-core/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(unexpected_cfgs, deprecated, ambiguous_glob_reexports)]
+
 pub mod constants;
 pub mod error;
 pub mod instructions;
@@ -15,7 +17,11 @@ declare_id!("6KQmaV2t8mesPNWAmn4cWJY4FnwAAzqQDbzGET9Km5G2");
 pub mod anchor_marketplace_core {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn initialize(ctx: Context<Initialize>, name: String, fee: u16) -> Result<()> {
+        ctx.accounts.intitialize(name, fee, &ctx.bumps)
+    }
+
+    pub fn list(ctx: Context<List>, price: u64) -> Result<()> {
+        ctx.accounts.create_listing(price, &ctx.bumps)
     }
 }
